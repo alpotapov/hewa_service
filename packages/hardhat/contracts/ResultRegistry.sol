b465476a55156f5d460840fc148b6109cfa88d7b
Alexander Potapov <al.potapov@hotmail.com>
2022-10-16 22:22:54 +0200

    Implement method for publishing result

--- packages/hardhat/contracts/ResultRegistry.sol
+++ packages/hardhat/contracts/ResultRegistry.sol
@@ -7,10 +7,12 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./VerifySignature.sol";
 
-contract ResultRegistry is Ownable {
+contract ResultRegistry is Ownable, VerifySignature {
   mapping(address => bool) private authorizedDevices;
+  mapping(string => string) private results;
 
   event Authorized(address device);
+  event ResultPublished(string guid);
 
   constructor () {}
 
@@ -24,5 +26,32 @@ contract ResultRegistry is Ownable {
 
   function removeDeviceAuthorization(address _device) public onlyOwner() {
     authorizedDevices[_device] = false;
+  }
+
+  function isDeviceAuthorized(address _device) public view returns (bool) {
+    return authorizedDevices[_device];
+  }
+
+  function publishResult(
+    address _device,
+    string memory _guid,
+    string memory _result,
+    uint _nonce,
+    bytes memory _signature
+  ) external {
+    bytes memory result = bytes(results[_guid]);
+    require(result.length == 0, "ResultRegistry: guid already in use");
+
+    require(
+      isDeviceAuthorized(_device),
+      "ResultRegistry: device not authorized"
+    );
+    require(
+      verify(_device, _result, _nonce, _signature),
+      "ResultRegistry: wrong signature"
+    );
+
+    results[_guid] = _result;
+    emit ResultPublished(_guid);
   }
 }
--- packages/hardhat/test/resultRegistryTest.js
+++ packages/hardhat/test/resultRegistryTest.js
@@ -10,6 +10,13 @@ const prepareTest = async () => {
   };
 };
 
+const signResults = async (signer, contract, nonce, results) => {
+  const hash = await contract.getMessageHash(results, nonce);
+  const signature = await signer.signMessage(ethers.utils.arrayify(hash));
+
+  return { signature, nonce };
+};
+
 describe("ResultRegistry", () => {
   before((done) => {
     setTimeout(done, 2000);
@@ -36,6 +43,35 @@ describe("ResultRegistry", () => {
         contract,
         "Authorized"
       );
+    });
+
+    it("should save result from the authorized device", async () => {
+      const { contract } = await prepareTest();
+      const [, , device1] = await ethers.getSigners();
+
+      await contract.authorizeDevice(device1.address);
+
+      const guid = "12345abc-12454adaf-1235435adasdf";
+      const nonce = 1;
+      const results = "0.12345";
+      const { signature } = await signResults(
+        device1,
+        contract,
+        nonce,
+        results
+      );
+
+      await expect(
+        await contract.publishResult(
+          device1.address,
+          guid,
+          results,
+          nonce,
+          signature
+        )
+      )
+        .to.emit(contract, "ResultPublished")
+        .withArgs(guid);
     });
   });
 });
