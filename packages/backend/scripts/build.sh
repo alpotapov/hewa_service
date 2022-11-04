7fc1dc6ad873af37a968f01fa5856d7b6e6e7e28
Alexander Potapov <alexander.potapov@check24.de>
2022-11-04 00:47:26 +0000

    Fix an issue with npm install failing during docker build due to wrong network settings

--- packages/backend/scripts/build.sh
+++ packages/backend/scripts/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build --tag alpotapov/hewa_service:latest --platform linux/amd64 .
+docker build --network host --tag alpotapov/hewa_service:latest --platform linux/amd64 .
 
 docker tag alpotapov/hewa_service registry.heroku.com/hewa-service/web
