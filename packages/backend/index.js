022d6fa5aed4f2edaa15e35c0977e7adaeea6603
Alexander Potapov <al.potapov@hotmail.com>
2022-12-08 21:44:37 +0100

    Add config for polygon. Log incoming requests

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -3,7 +3,6 @@ const path = require('path');
 const config = require('config');
 const cors = require('cors');
 
-const healthWalletAccessApi = require('./src/api/healthWalletAccess');
 const deviceApi = require('./src/api/device');
 const resultApi = require('./src/api/result');
 
@@ -13,10 +12,13 @@ const CHAIN_NAME = config.get('chainName');
 
 const app = express();
 app.use(cors());
+app.use((req, res, next) => {
+  console.log(`${Date.now()} ${req.method} ${req.url} ${JSON.stringify(req.body)}`);
+  next();
+});
 app.use(express.json({ extended: true }));
 app.use('/data', express.static(path.join(__dirname, 'public')));
 
-app.use('/api/v1/access', healthWalletAccessApi);
 app.use('/api/v1/device', deviceApi);
 app.use('/api/v1/result', resultApi);
 
