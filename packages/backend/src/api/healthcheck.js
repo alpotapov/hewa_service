d58fd3666c3be796e2ed82e739bbd2427ff787f4
Alexander Potapov <al.potapov@hotmail.com>
2023-01-28 18:21:03 +0100

    Healthcheck endpoint

--- packages/backend/src/api/healthcheck.js
+++ packages/backend/src/api/healthcheck.js
@@ -6,4 +6,11 @@ router.get('/', async (req, res) => {
   res.json({ status: 'ok' });
 });
 
+router.post('/', async (req, res) => {
+  // const { guid, result } = req.body;
+  console.log(JSON.stringify(req.body, null, 2));
+
+  res.status(200).send();
+});
+
 module.exports = router;
--- packages/backend/utils/requestNewAccessToken.js
+++ packages/backend/utils/requestNewAccessToken.js
@@ -1,7 +1,7 @@
+const ethers = require('ethers');
 
-const ethers = require('ethers')
-const CHAIN_ID = "31337";
-const CHAIN_NAME = "localhost";
+const CHAIN_ID = '31337';
+const CHAIN_NAME = 'localhost';
 const artifacts = require('../src/contracts/hardhat_contracts.json');
 
 const { HealthWalletAccess } = artifacts[CHAIN_ID][CHAIN_NAME].contracts;
@@ -9,18 +9,17 @@ const rpcUrl = 'http://localhost:8545';
 const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
 const { abi: hwaABI, address: hwaAddress } = HealthWalletAccess;
 
-
 const go = async (pk) => {
   const contract = new ethers.Contract(hwaAddress, hwaABI, provider);
-  const hash = await contract.getMessageHash("Create HealthWalletAccess token", 1);
-  
+  const hash = await contract.getMessageHash('Create HealthWalletAccess token', 1);
+
   const wallet = new ethers.Wallet(pk);
 
   const signed = await wallet.signMessage(ethers.utils.arrayify(hash));
 
   return signed;
-}
+};
 
 module.exports = {
-  go
-}
+  go,
+};
